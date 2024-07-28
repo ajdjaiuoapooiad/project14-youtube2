03dc29b23e31c76d28b90478482a32ac753b2a29
user <user@usernoMacBook-Air.local>
2024-07-28 15:41:29 +0900

    model Post

--- Youtube/models.py
+++ Youtube/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField('タイトル',max_length=50)
+    text=models.TextField('詳細')
+    thumbnail=models.ImageField('画像',upload_to='media/thumbnail')
+    movie=models.FileField('動画',upload_to='uploads/%Y/%m/%d/')
+    created_at=models.DateTimeField('作成日',auto_now_add=True)
+    good=models.IntegerField('高評価')
+    read=models.IntegerField('閲覧数')
+    usertext=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
